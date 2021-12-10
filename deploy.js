58375c901265222b543cf7fe4b65ac54c6057c83
cmembreno2 <carlosmembreno2@hotmail.com>
2021-12-09 22:19:41 -0600

    my first commit

---
+++ deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+ 
+const { abi, evm } = require('./compile');
+ 
+provider = new HDWalletProvider(
+  'type toward enforce onion runway wolf cream ladder rate enemy other magnet',
+  'https://rinkeby.infura.io/v3/bbd419044d21408eb72ccc23dff452c1'
+);
+ 
+const web3 = new Web3(provider);
+ 
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+ 
+  console.log('Attempting to deploy from account', accounts[0]);
+ 
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+ 
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+ 
+deploy();
